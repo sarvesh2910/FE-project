e7858bf2ea2bd5a0b8612cf9d5daa3b1948e11c5
George Olson <golson@pdx.edu>
2023-11-28 06:45:48 -0800

    Getting coordinates for Google maps

--- f1-stats/src/app/page.jsx
+++ f1-stats/src/app/page.jsx
@@ -4,8 +4,27 @@ import { React, useState, useEffect } from 'react';
 const Home = () => {
 
     const [circuit, setCircuit] = useState({circuitName: '', url: ''});
+    const [circuitCoordinates, setCircuitCoordinates] = useState({lat: '', lon: ''});
     const circuitUrl = `https://ergast.com/api/f1/current.json`;
+    const wikipediaUrl = `https://en.wikipedia.org/w/api.php?action=query&prop=coordinates&titles=${circuit.circuitName}`;
 
+    const getCoordinateData = async function fetchCoordinateDataFromURL(endpoint) {
+        console.log(endpoint);
+        try {
+            const response = await fetch(endpoint);
+
+            // Now turn data into a json readable format
+            const data = await response.json();
+            console.log(data);
+            console.log(data['query']['pages'][Object.keys(data['query']['pages'])[0]]['coordinates'][0]);
+            setCircuitCoordinates(data['query']['pages'][Object.keys(data['query']['pages'])[0]]['coordinates'][0])
+
+        } catch (error) {
+            // Error from API fetch
+            console.error('Request failed', error);
+        } 
+    }
+    
     // Fetch data from API asyncronously
     const getCircuitData = async function fetchCircuitDataFromURL(endpoint) {
         console.log(endpoint);
@@ -18,6 +37,10 @@ const Home = () => {
             console.log(races);
             console.log(races[races.length - 1]['Circuit']);
             setCircuit(races[races.length - 1]['Circuit']);
+            const circuitName = races[races.length - 1]['Circuit']['circuitName'];
+
+            const wikiUrl = `https://en.wikipedia.org/w/api.php?action=query&prop=coordinates&titles=${circuitName}&origin=*&format=json`
+            getCoordinateData(wikiUrl);
 
         } catch (error) {
             // Error from API fetch
@@ -39,6 +62,9 @@ const Home = () => {
                         <h2>{circuit.circuitName}</h2>
                         <a href={circuit.url} target='_blank'>View on Wikipedia</a>
                         <p>Content about race</p>
+                        <p>{wikipediaUrl}</p>
+                        <p>Lat: {circuitCoordinates.lat}</p>
+                        <p>Lon: {circuitCoordinates.lon}</p>
                     </div>
                     <div className="col-md-4">
                         <h2>Map</h2>
