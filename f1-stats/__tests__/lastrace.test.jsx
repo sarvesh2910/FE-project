ae71ec51ecf9b58c75a43b114700aad7cb2e60a6
George Olson <golson@pdx.edu>
2023-11-29 12:03:47 -0800

    Added some tests & snapshot test

--- f1-stats/__tests__/lastrace.test.jsx
+++ f1-stats/__tests__/lastrace.test.jsx
@@ -1,12 +1,12 @@
 // __tests__/index.test.js
 
 import { render, screen } from '@testing-library/react';
-import Home from '@/app/page';
+import LastRace from '@/app/components/lastrace';
 import '@testing-library/jest-dom';
 
-describe('Home', () => {
+describe('LastRace', () => {
   it('Renders component heading', () => {
-    render(<Home />)
+    render(<LastRace />)
 
     const heading = screen.getByRole('heading', {
       name: "Most Recent Race:",
