2c23b3e3fc0c800df553c24579f46133f7990a05
George Olson <golson@pdx.edu>
2023-11-29 18:13:10 -0800

    Minor update to loading

--- f1-stats/src/app/components/lastrace.jsx
+++ f1-stats/src/app/components/lastrace.jsx
@@ -82,7 +82,9 @@ const LastRace = () => {
     };
 
     useEffect(() => {
-        getCircuitData(circuitUrl).then(setLoading(false));
+        getCircuitData(circuitUrl).then(setTimeout(() => {
+            setLoading(false);
+          }, "1500"));
         console.log(process.env.GOOGLE_API_KEY);
     }, []);
 
