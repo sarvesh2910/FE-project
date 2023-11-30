04e1676f5a77167733a05c0ebfb773031cfbdc8e
Ruskidre <andrey8@pdx.edu>
2023-11-29 18:48:18 -0800

    fixed up search bars

--- f1-stats/src/app/drivers/page.jsx
+++ f1-stats/src/app/drivers/page.jsx
@@ -1,52 +1,54 @@
-"use client"; // This is a client component
+'use client'
 import React, { useState, useEffect } from 'react';
 
 function DriversList() {
-  const [drivers, setDrivers] = useState([]);
-  const [selectedDriver, setSelectedDriver] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [searchQuery, setSearchQuery] = useState('');
-  const [yearFilter, setYearFilter] = useState('');
+    const [drivers, setDrivers] = useState([]);
+    const [selectedDriver, setSelectedDriver] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [yearFilter, setYearFilter] = useState('');
+    const [showDropdown, setShowDropdown] = useState(false);
 
-  useEffect(() => {
-    const fetchDrivers = async () => {
-      if (yearFilter.length === 4) {
-        try {
-          setLoading(true);
-          const response = await fetch(`http://ergast.com/api/f1/${yearFilter}/drivers.json`);
-
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-
-          const data = await response.json();
-          setDrivers(data.MRData.DriverTable.Drivers);
-        } catch (error) {
-          setError('Failed to load drivers. Please check the year.');
-        } finally {
-          setLoading(false);
+    useEffect(() => {
+        if (yearFilter.length === 4) {
+            setLoading(true);
+            fetch(`http://ergast.com/api/f1/${yearFilter}/drivers.json`)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    setDrivers(data.MRData.DriverTable.Drivers);
+                    setLoading(false);
+                })
+                .catch(error => {
+                    setError('Failed to load drivers. Please check the year.');
+                    setLoading(false);
+                });
+        } else {
+            setDrivers([]);
+            setError(null);
         }
-      } else {
-        setDrivers([]);
-        setError(null);
-      }
+    }, [yearFilter]);
+
+    const handleDriverClick = (driver) => {
+        setSelectedDriver(driver);
+        setSearchQuery(`${driver.givenName} ${driver.familyName}`);
+        setShowDropdown(false);
     };
 
-    fetchDrivers();
-  }, [yearFilter]);
+    const handleSearchChange = (e) => {
+        setSearchQuery(e.target.value);
+        setShowDropdown(true);
+        setSelectedDriver(null); // Clear selected driver when search changes
+    };
 
-  const handleDriverClick = (driver) => {
-    setSelectedDriver(driver);
-  };
-
-  const filteredDrivers = drivers.filter((driver) => 
-    `${driver.givenName} ${driver.familyName}`.toLowerCase().includes(searchQuery.toLowerCase())
-  );
-
-  if (loading) return <p>Loading drivers...</p>;
-  if (error) return <p>{error}</p>;
-
+    const filteredDrivers = drivers.filter(driver =>
+        `${driver.givenName} ${driver.familyName}`.toLowerCase().includes(searchQuery.toLowerCase())
+    );
 
     return (
         <div className="container">
@@ -60,42 +62,42 @@ function DriversList() {
                         onChange={(e) => setYearFilter(e.target.value)}
                     />
                 </div>
-                <div className="col-6 col-md-3">
+                <div className="col-6 col-md-3 position-relative">
                     <input
                         type="text"
                         className="form-control"
                         placeholder="Search for a driver"
                         value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}
+                        onChange={handleSearchChange}
+                        onFocus={() => setShowDropdown(true)}
                     />
+                    {showDropdown && (
+                        <div className="list-group w-100" style={{ maxHeight: '200px', overflowY: 'auto', zIndex: 1050 }}>
+                            {filteredDrivers.map(driver => (
+                                <button 
+                                    type="button"
+                                    key={driver.driverId} 
+                                    className="list-group-item list-group-item-action"
+                                    onClick={() => handleDriverClick(driver)}
+                                >
+                                    {driver.givenName} {driver.familyName}
+                                </button>
+                            ))}
+                        </div>
+                    )}
                 </div>
             </div>
             {loading && <p>Loading drivers...</p>}
             {error && <p>{error}</p>}
-            {!loading && yearFilter.length === 4 && (
-                <div className="row">
-                    <div className="col">
-                        <h2>F1 Drivers in {yearFilter}</h2>
-                        <ul className="list-group">
-                            {filteredDrivers.map(driver => (
-                                <li 
-                                    key={driver.driverId} 
-                                    className="list-group-item list-group-item-action"
-                                    onClick={() => handleDriverClick(driver)}>
-                                    {driver.givenName} {driver.familyName}
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                </div>
-            )}
             {selectedDriver && (
                 <div className="row mt-3">
                     <div className="col">
-                        <h3>Driver Details</h3>
-                        <p>Name: {selectedDriver.givenName} {selectedDriver.familyName}</p>
-                        {/* Additional driver details can be added here */}
-                    </div>
+            <h3>Driver Details</h3>
+            <p><strong>Name:</strong> {selectedDriver.givenName} {selectedDriver.familyName}</p>
+            <p><strong>Date of Birth:</strong> {selectedDriver.dateOfBirth}</p>
+            <p><strong>Nationality:</strong> {selectedDriver.nationality}</p>
+            <p><strong>Wikipedia:</strong> <a href={selectedDriver.url} >Link</a></p>
+        </div>
                 </div>
             )}
         </div>
